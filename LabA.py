56f5bf90d9f6c33f88c9fdf1f2c63419774bfbe5
GlitchDino <39248385+GlitchDino@users.noreply.github.com>
2022-02-15 15:10:34 -0500

    Add files via upload
    V2

--- LabA.py
+++ LabA.py
@@ -1,35 +1,46 @@
 from posixpath import split
 import numpy as np
-from numpy import array
+class Mouse:
+    mouse_pos=(0,0)
+    cheese_location=[]
 
+    def print_maze(self, file):
+        read_file=open(file, "r")
+        lines=read_file.readlines()
+        width=len(lines[0])-1 #-1 for new line
+        height=len(lines)
+        for line in lines:
+            print(line)
 
-def print_maze(file):
-    read_file=open(file, "r")
-    lines=read_file.readlines()
-    width=len(lines[0])-1 #-1 for new line
-    height=len(lines)
-    for line in lines:
-        print(line)
+    def init_maze(self, file):
+        read_file=open(file, "r")
+        lines=read_file.readlines()
+        width=len(lines[0])-1 #-1 for new line
+        height=len(lines)
+        print("Width of maze: "+str(width)+" Height of maze: "+str(height))
+        maze_arr=[]
+        row_index=0
 
-def init_maze(file):
-    read_file=open(file, "r")
-    lines=read_file.readlines()
-    width=len(lines[0])-1 #-1 for new line
-    height=len(lines)
-    print("Width of maze: "+str(width)+" Height of maze: "+str(height))
-    maze_arr=[]
-    for line in lines:
-        row=[]
-        
-        split_str=line.split("\n")
-        row.append(list(split_str[0]))
-        maze_arr.append(row)
-    numpy_maze = np.array(maze_arr)
-    mouse_location=np.where(numpy_maze=="P")
-    cheese_location=np.where(numpy_maze==".")
-    print(mouse_location)
-    print(cheese_location)
+        for line in lines:
+            row=[]
+            split_str=line.split("\n")
+            row.append(list(split_str[0]))
+            maze_arr.append(row)
+            if "." in row:
+                print("WOOP")
+            #for character in row:
+            #    if character=='.':
+            #        print("CHEESE FOUND!!!")
+            row_index+=1
+        numpy_maze = np.array(maze_arr)
+        mouse_location=np.where(numpy_maze=="P")
+        cheese_location=np.where(numpy_maze==".") #Only manages location of 1 cheese
+        print(mouse_location)
+        self.mouse_pos=(mouse_location[0][0], mouse_location[2][0])
+        print(self.mouse_pos)
+        print(cheese_location)
 
-init_maze("/Users/egray/Desktop/SCHOOL2022/AI/1prize-medium.txt")
+test=Mouse()
+test.init_maze("/Users/egray/Desktop/SCHOOL2022/AI/1prize-medium.txt")
 #print_maze("/Users/egray/Desktop/SCHOOL2022/AI/1prize-medium.txt")
 
